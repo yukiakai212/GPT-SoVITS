619570dfaab2fded938ea44846f6896f65794243
Yuki Akai <yukiakai212@gmail.com>
2024-12-15 21:49:10 +0700

    refactor: gradio with conda

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ conda install -c conda-forge gxx -y
 conda install ffmpeg cmake -y
 conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia -y
 pip install -r requirements.txt
-#for python 3.10.4, tested with colab
-conda install pytorch-lightning torchmetrics -y
+conda install conda-forge::gradio -y
+#for python 3.10.4
+#conda install pytorch-lightning torchmetrics -y
